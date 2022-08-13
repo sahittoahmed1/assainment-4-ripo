e77f88fb91086f5e82104784dbfe8f8f82d78486
sahittoahmed1 <sahittoahmed039@gmail.com>
2022-08-13 21:56:49 +0600

    secend javascript commit

---
+++ geven-file.js
@@ -0,0 +1,11 @@
+
+// 2.given file
+const isJavascriptFile = 'javsscript';
+
+ if(isJavascriptFile === 'javsscript'){
+    console.log(true);
+    return false;
+}
+else{
+    console.log(false);
+}
